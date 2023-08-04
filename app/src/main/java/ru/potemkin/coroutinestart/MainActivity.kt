aff4d673f96deea7f466b4b406486524e97a0f3b
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-04 14:21:36 +0300

    Async and Deferred

--- app/src/main/java/ru/potemkin/coroutinestart/MainActivity.kt
+++ app/src/main/java/ru/potemkin/coroutinestart/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import ru.potemkin.coroutinestart.databinding.ActivityMainBinding
@@ -25,17 +27,24 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.setOnClickListener {
             binding.progress.isVisible = true
             binding.buttonLoad.isEnabled = false
-            val jobCity = lifecycleScope.launch {
+            val deferredCity: Deferred<String> = lifecycleScope.async {
                 val city = loadCity()
                 binding.tvLocation.text = city
+                city
             }
-            val jobTemp = lifecycleScope.launch {
+            val deferredTemp:Deferred<Int> = lifecycleScope.async {
                 val temp = loadTemperature()
                 binding.tvTemperature.text = temp.toString()
+                temp
             }
-            lifecycleScope.launch {
-                jobCity.join()
-                jobTemp.join()
+            lifecycleScope.async {
+                val city = deferredCity.await()
+                val temp = deferredTemp.await()
+                Toast.makeText(
+                    this@MainActivity,
+                    "City $city, Temp $temp",
+                    Toast.LENGTH_SHORT)
+                    .show()
                 binding.progress.isVisible = false
                 binding.buttonLoad.isEnabled = true
             }
