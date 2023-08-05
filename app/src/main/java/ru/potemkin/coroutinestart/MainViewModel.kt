95c49c0cd70102b60f6534c5041054a63cd4982e
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-05 14:56:19 +0300

    Exception Handling

--- app/src/main/java/ru/potemkin/coroutinestart/MainViewModel.kt
+++ app/src/main/java/ru/potemkin/coroutinestart/MainViewModel.kt
@@ -7,37 +7,40 @@ import kotlin.concurrent.thread
 
 class MainViewModel : ViewModel() {
     private val parentJob = Job()
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.d(LOG_TAG, "Exception caught $throwable")
+    }
 
-    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob + exceptionHandler)
     fun method() {
         val childJob1 = coroutineScope.launch {
             delay(3000)
-            Log.d(LOG_TAG,"First coroutine finished")
+            Log.d(LOG_TAG, "First coroutine finished")
         }
 
         val childJob2 = coroutineScope.launch {
             delay(2000)
-            childJob1.cancel()
-            Log.d(LOG_TAG,"Second coroutine finished")
-            Log.d(LOG_TAG,"Parent coroutine cancelled: ${parentJob.isCancelled}")
+            Log.d(LOG_TAG, "Second coroutine finished")
         }
-//        thread {
-//            Thread.sleep(1000)
-//            parentJob.cancel()
-//            Log.d(LOG_TAG,"Parent job active: ${parentJob.isActive}")
-//        }
-        Log.d(LOG_TAG,parentJob.children.contains(childJob1).toString())
-        Log.d(LOG_TAG,parentJob.children.contains(childJob2).toString())
+        val childJob3 = coroutineScope.launch {
+            delay(1000)
+            error()
+            Log.d(LOG_TAG, "Third coroutine finished")
+        }
+
     }
 
-    override fun onCleared(){
+    private fun error() {
+        throw java.lang.RuntimeException()
+    }
+
+    override fun onCleared() {
         super.onCleared()
         coroutineScope.cancel()
     }
 
 
-
-    companion object{
+    companion object {
         private const val LOG_TAG = "MainViewModel"
     }
 }
